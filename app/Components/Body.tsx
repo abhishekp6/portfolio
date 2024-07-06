aeb4ff4bd823bce9b3ad9dd7153b418249366fa9
abhishekp6 <53012981+abhishekp6@users.noreply.github.com>
2024-07-06 23:07:04 +0530

    Fix project section responsiveness in phone screen

--- app/Components/Body.tsx
+++ app/Components/Body.tsx
@@ -72,44 +72,45 @@ const Body = () => {
             </div>
             <div id="projects" className="mt-24">
                 <div className="text-2xl font-bold">Projects</div>
-                <div className="flex flex-wrap mt-4 h-fit">
-                    <div className="border-solid border-2 w-96 mr-12 p-1">
+                <div className="flex flex-wrap space-x-2 space-y-2">
+                    <div></div>
+                    <div className="border-solid border w-full sm:w-auto p-1 rounded-md border-neutral-400 flex-1">
                         <Image src="/learnest.png" alt="project" width="500" height="100"></Image>
                         <div className="text-lg my-6 flex justify-center items-center font-bold">Learnest</div>
-                        <div className="mx-2 mb-10 text-justify">
+                        <div className="mb-10 text-justify mx-2">
                         Learnest is a web application designed for individual course creators. It enables you to create, manage, and display video tutorials to your viewers.
                         </div>
                         <div className="flex">
-                            <div className="w-24 h-8 mx-2 border-2 flex items-center justify-center text-slate-600 cursor-pointer">
+                            <div className="w-24 h-8 mx-2 border rounded-md border-neutral-400 flex items-center justify-center text-slate-600 cursor-pointer">
                                 <Link href="https://github.com/abhishekp6/learnest" target="_blank">Github</Link>
                                 <span className="ml-2"><svg viewBox="0 0 16 16" className="w-5 h-5" fill="currentColor" aria-hidden="true"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg></span>
                             </div>
-                            <div className="w-24 h-8 mx-2 border-2 flex items-center justify-center text-slate-600 cursor-pointer">
+                            <div className="w-24 h-8 mx-2 border rounded-md border-neutral-400 flex items-center justify-center text-slate-600 cursor-pointer">
                             <Link href="https://learnest-fe.vercel.app" target="_blank">Live</Link>
                             </div>
                         </div>
                     </div>
-                    <div className="border-solid border-2 w-96 mr-12 p-1">
+                    <div className="border-solid border rounded-md border-neutral-400 w-full sm:w-auto p-1 rounded-md flex-1">
                         <Image src="/yb.png" alt="project" width="500" height="100"></Image>
                         <div className="text-lg my-6 flex justify-center items-center font-bold">Youtube Browser</div>
                         <div className="text-justify mx-2">
                         It is a simple youtube wrapper, which fetches data from Google&apos;s youtube data API based on search term and plays in an iframe. The application is solely created with React and Semantic UI.
                         </div>
-                        <div className="flex mt-4">
-                            <div className="w-24 h-8 mx-2 border-2 flex items-center justify-center text-slate-600 cursor-pointer">
+                        <div className="flex mt-10">
+                            <div className="w-24 h-8 mx-2 border rounded-md border-neutral-400 flex items-center justify-center text-slate-600 cursor-pointer">
                             <Link href="https://github.com/abhishekp6/youtube-browser" target="_blank">Github</Link>
                             <span className="ml-2"><svg viewBox="0 0 16 16" className="w-5 h-5" fill="currentColor" aria-hidden="true"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg></span>
                             </div>
                         </div>
                     </div>
-                    <div className="border-solid border-2 w-96 p-1">
+                    <div className="border-solid border rounded-md border-neutral-400 w-full sm:w-auto p-1 rounded-md flex-1">
                         <Image src="/live-editor.png" alt="project" width="500" height="100"></Image>
                         <div className="text-lg my-6 flex justify-center items-center font-bold">Live Editor</div>
                         <div className="text-justify mx-2">
                         It&apos;s live code editor with real-time output for HTML, CSS, and JavaScript, served by an Express server.
                         </div>
                         <div className="flex mt-16">
-                            <div className="w-24 h-8 mx-2 border-2 flex items-center justify-center text-slate-600 cursor-pointer">
+                            <div className="w-24 h-8 mx-2 border rounded-md border-neutral-400 flex items-center justify-center text-slate-600 cursor-pointer">
                             <Link href="https://github.com/abhishekp6/live-js-editor" target="_blank">Github</Link>
                             <span className="ml-2"><svg viewBox="0 0 16 16" className="w-5 h-5" fill="currentColor" aria-hidden="true"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg></span>
                             </div>
