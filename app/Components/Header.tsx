93431c35dcd75d4dcc9bc1b19716b2dd50576470
abhishekp6 <53012981+abhishekp6@users.noreply.github.com>
2024-08-03 15:29:28 +0530

    Build fix

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
     return(
             <div id="header" className="h-96 flex flex-col justify-center items-center">
                 <div className="text-7xl font-bold text-center w-full md:w-auto p-4">
-                    Hi, I'm Abhishek 👋
+                    Hi, I&apos;m Abhishek 👋
                 </div>
                 <div className="text-2xl mt-2 text-slate-500">
                 Software Engineer | Full Stack Developer
