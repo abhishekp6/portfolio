72a5a678b28030e448847b51bbdf91ef26738868
abhishekp6 <53012981+abhishekp6@users.noreply.github.com>
2024-08-03 15:35:12 +0530

    Fix small screen left paddings

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -1,14 +1,14 @@
 const Header = () => {
-    return(
-            <div id="header" className="h-96 flex flex-col justify-center items-center">
-                <div className="text-7xl font-bold text-center w-full md:w-auto p-4">
-                    Hi, I&apos;m Abhishek 👋
-                </div>
-                <div className="text-2xl mt-2 text-slate-500">
-                Software Engineer | Full Stack Developer
-                </div>
-            </div>
-    );
-}
+  return (
+    <div id="header" className="h-96 flex flex-col justify-center items-center">
+      <div className="text-7xl font-bold text-center w-full md:w-auto p-4">
+        Hi, I&apos;m Abhishek 👋
+      </div>
+      <div className="text-2xl mt-2 text-slate-500 text-center sm:ml-8">
+        Software Engineer | Full Stack Developer
+      </div>
+    </div>
+  );
+};
 
 export default Header;
