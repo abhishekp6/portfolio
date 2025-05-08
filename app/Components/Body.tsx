e800dad911a8d6405dced2cc2f6a50f7708068c9
abhishekp6 <53012981+abhishekp6@users.noreply.github.com>
2025-05-08 09:18:56 +0530

    fix: add fade in on initial view

--- app/Components/Body.tsx
+++ app/Components/Body.tsx
@@ -13,7 +13,8 @@ const useFadeInOnScroll = () => {
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
-            entry.target.classList.add("opacity-100", "translate-y-0");
+            entry.target.classList.remove('opacity-0', 'translate-y-10');
+            entry.target.classList.add('opacity-100', 'translate-y-0');
           }
         });
       },
